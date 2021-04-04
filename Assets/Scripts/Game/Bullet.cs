ad373dec50b27987c2ad4edf889cb831565db1ed
6a3yka <pr0ivdm@yandex.ru>
2021-04-04 14:33:57 +0700

    Shooting enemy implementation

---
+++ Assets/Scripts/Game/Bullet.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Vector3 direction = Vector3.left;
+    public float speed = 10.0f;
+
+    private void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Weapon"))
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Game/ShootingEnemy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Game
+{
+    class ShootingEnemy : Enemy
+    {
+        public GameObject bullet;
+
+        private void Start()
+        {
+            StartCoroutine(ShootCoroutine());
+        }
+
+        IEnumerator ShootCoroutine()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(1.5f);
+                Instantiate(bullet);
+            }
+        }
+    }
+}
