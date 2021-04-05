8526817fed50b77256c1a06e11ce9e906002d479
6a3yka <pr0ivdm@yandex.ru>
2021-04-05 21:32:49 +0700

    Implement simple DebugUI

---
+++ Assets/Scripts/UI/DebugUi.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Scripts.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    public class DebugUi : MonoBehaviour
+    {
+        private void OnGUI()
+        {
+            if (GUILayout.Button("Print save json"))
+            {
+                var json = JsonUtility.ToJson(GameData.Instance);
+                Debug.Log(json);
+            }
+        }
+    }
+}
