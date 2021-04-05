3d11bab80b03e6ca75108d762ee8342d39b6630b
6a3yka <pr0ivdm@yandex.ru>
2021-04-05 17:43:09 +0700

    Display coin count on UI

---
+++ Assets/Scripts/UI/CoinText.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Scripts.Game;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinText : MonoBehaviour
+{
+    private Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = GameData.Instance.coins.ToString() + " у.е.";
+    }
+}
