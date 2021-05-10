87924059e2d7aa5318a6ba3be74cc170c2bad6d5
cvertee <cvertee@gmail.com>
2021-05-10 22:44:45 +0700

    Key scriptable object

---
+++ Assets/Scripts/Game/Key.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Key", menuName = "Keys/Key", order = 1)]
+public class Key : ScriptableObject
+{
+    public string id;
+    public string description;
+}
