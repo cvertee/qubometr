c4132c68ec4a852419f41637ad97357d07941fde
cvertee <cvertee@gmail.com>
2021-05-10 22:59:12 +0700

    Check if key was picked up before

--- Assets/Scripts/Game/KeyPickup.cs
+++ Assets/Scripts/Game/KeyPickup.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class KeyPickup : MonoBehaviour, IInteractable
 {
     public Key key;
+
+    private void Awake()
+    {
+        if (GameData.Data.pickedUpKeys.Any(x => x.id == key.id))
+            Destroy(gameObject);
+    }
     
     public void Interact()
     {
