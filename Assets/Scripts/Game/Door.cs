cb83c0395f62f448043aea9112c4c6778231025d
cvertee <cvertee@gmail.com>
2021-05-10 22:46:01 +0700

    Make door require keys

--- Assets/Scripts/Game/Door.cs
+++ Assets/Scripts/Game/Door.cs
@@ -7,11 +7,29 @@ using UnityEngine.SceneManagement;
 public class Door : MonoBehaviour, IInteractable
 {
     public string sceneName;
+    public Key requiredKey; 
     
     public void Interact()
     {
         if (string.IsNullOrEmpty(sceneName))
+        {
+            Debug.Log($"Door: No scene name in {name}");
             return;
+        }
+
+        if (requiredKey != null)
+        {
+            if (GameData.Data.pickedUpKeys.Contains(requiredKey))
+            {
+                SceneManager.LoadScene(sceneName);
+                return;
+            }
+            else
+            {
+                Debug.Log($"Door: No key! (requires {requiredKey.id})");
+                return;
+            }
+        }
 
         SceneManager.LoadScene(sceneName);
     }
