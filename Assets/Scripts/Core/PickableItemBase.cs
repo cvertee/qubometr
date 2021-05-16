936d35e1c5f6ca494027a16c25ba6edd9704514a
cvertee <cvertee@gmail.com>
2021-05-16 17:31:40 +0700

    On pickup events for pickable items

--- Assets/Scripts/Core/PickableItemBase.cs
+++ Assets/Scripts/Core/PickableItemBase.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PickableItemBase : DestroyerBase, IInteractable
 {
+    public UnityEvent onPickup;
+    
     public void Interact()
     {
+        if (onPickup != null)
+            onPickup.Invoke();
+        
         OnPickup();
     }
 
